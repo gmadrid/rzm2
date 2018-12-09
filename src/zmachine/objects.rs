bfdd62aac565fa30938ab97eb72a0419ae898535
George Madrid <gmadrid@gmail.com>
2018-12-09 07:56:18 -0500

    Work in progress on Objects.

--- src/zmachine/objects.rs
+++ src/zmachine/objects.rs
@@ -20,58 +20,37 @@ use super::version::ZVersion;
 // print_obj o       - print the short name of object
 // put_prop o p v    - write value to property in object
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Object {
-    Null,
-    Address(ByteAddress),
-}
+pub struct ObjectNumber(u16);
 
-impl Object {
-    fn with_address<F, T>(&self, f: F) -> Result<T>
-    where
-        F: FnOnce(ByteAddress) -> Result<T>,
-    {
-        // TODO: range check
-        match self {
-            Object::Null => Err(ZErr::NullObject),
-            Object::Address(ba) => f(*ba),
-        }
-    }
-
-    fn offset(&self, offset: u16) -> Result<ByteAddress> {
-        self.with_address(|ba| Ok(ba.inc_by(offset)))
-    }
-}
-
-impl From<Object> for ZOffset {
-    fn from(obj: Object) -> ZOffset {
-        match obj {
-            Object::Null => panic!("Taking address of Null object."),
-            Object::Address(ba) => ba.into(),
-        }
-    }
-}
+pub trait Object {}
 
 pub trait ObjectTable {
+    type O: Object;
+
     // TODO: range check.
-    //    fn get_object(&self, num: u16);
+    fn get_object(&self, num: ObjectNumber) -> Result<Self::O>;
 
-    fn get_object_child(&self, o: Object) -> Result<Object>;
-    fn get_object_sibling(&self, o: Object) -> Result<Object>;
-    fn get_object_parent(&self, o: Object) -> Result<Object>;
+    fn get_object_child(&self, o: Self::O) -> Result<ObjectNumber>;
+    fn get_object_sibling(&self, o: Self::O) -> Result<ObjectNumber>;
+    fn get_object_parent(&self, o: Self::O) -> Result<ObjectNumber>;
 
-    fn set_object_child(&self, o: Object, c: Object) -> Result<()>;
-    fn set_object_sibling(&self, o: Object, s: Object) -> Result<()>;
-    fn set_object_parent(&self, o: Object, p: Object) -> Result<()>;
+    fn set_object_child(&self, o: Self::O, c: ObjectNumber) -> Result<()>;
+    fn set_object_sibling(&self, o: Self::O, s: ObjectNumber) -> Result<()>;
+    fn set_object_parent(&self, o: Self::O, p: ObjectNumber) -> Result<()>;
 
-    fn get_object_attribute(&self, o: Object, a: u16) -> Result<u16>;
-    fn set_object_attribute(&self, o: Object, a: u16, v: u16) -> Result<()>;
+    fn get_object_attribute(&self, o: Self::O, a: u8) -> Result<u8>;
+    fn set_object_attribute(&self, o: Self::O, a: u8, v: u8) -> Result<()>;
 
-    fn get_object_property(&self, o: Object, p: u8) -> Result<u16>; // Is this right? Are all properties u16?
-    fn set_object_property(&self, o: Object, p: u8, v: u16) -> Result<()>;
+    fn get_object_property(&self, o: Self::O, p: u8) -> Result<u16>; // Is this right? Are all properties u16?
+    fn set_object_property(&self, o: Self::O, p: u8, v: u16) -> Result<()>;
 
     fn get_default_property(&self, p: u8) -> Result<u16>; // Is this right? Are all properties u16?
 }
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ZObject(ByteAddress);
+
+impl Object for ZObject {}
 
 pub struct ZObjectTable<M>
 where
@@ -103,57 +82,87 @@ where
             tree_offset: tree,
         }
     }
-
-    fn get_object(&self, num: u16) -> Object {
-        // TODO: range check
-        // VNUM DEPEND
-        // Objects are 1-indexed. (Zero is the null object.)
-        if num == 0 {
-            Object::Null
-        } else {
-            Object::Address(self.tree_offset.inc_by((num - 1) * 9))
-        }
-    }
 }
 
 impl<M> ObjectTable for ZObjectTable<M>
 where
     M: Memory,
 {
-    fn get_object_child(&self, o: Object) -> Result<Object> {
-        let offset = o.offset(6);
-        Ok(Object::Null)
+    type O = ZObject;
+
+    fn get_object(&self, num: ObjectNumber) -> Result<ZObject> {
+        // TODO: range check
+        // VNUM DEPEND
+        // Objects are 1-indexed. (Zero is the null object.)
+        if num.0 == 0 {
+            Err(ZErr::NullObject)
+        } else {
+            Ok(ZObject(self.tree_offset.inc_by((num.0 - 1) * 9)))
+        }
     }
 
-    fn get_object_sibling(&self, o: Object) -> Result<Object> {
-        panic!("Unimplemented")
-    }
-    fn get_object_parent(&self, o: Object) -> Result<Object> {
-        panic!("Unimplemented")
-    }
-
-    fn set_object_child(&self, o: Object, c: Object) -> Result<()> {
-        panic!("Unimplemented")
-    }
-    fn set_object_sibling(&self, o: Object, s: Object) -> Result<()> {
-        panic!("Unimplemented")
-    }
-    fn set_object_parent(&self, o: Object, p: Object) -> Result<()> {
-        panic!("Unimplemented")
+    // Consider returning Option here instead of an ObjectNumber(0).
+    fn get_object_child(&self, o: ZObject) -> Result<ObjectNumber> {
+        // VNUM DEPEND
+        let raw_number = self.memory.borrow().read_byte(ZOffset::from(o.0.inc_by(6)));
+        Ok(ObjectNumber(u16::from(raw_number)))
     }
 
-    fn get_object_attribute(&self, o: Object, a: u16) -> Result<u16> {
-        panic!("Unimplemented")
+    fn get_object_sibling(&self, o: ZObject) -> Result<ObjectNumber> {
+        // VNUM DEPEND
+        let raw_number = self.memory.borrow().read_byte(ZOffset::from(o.0.inc_by(5)));
+        Ok(ObjectNumber(u16::from(raw_number)))
     }
-    fn set_object_attribute(&self, o: Object, a: u16, v: u16) -> Result<()> {
-        panic!("Unimplemented")
+    fn get_object_parent(&self, o: ZObject) -> Result<ObjectNumber> {
+        // VNUM DEPEND
+        let raw_number = self.memory.borrow().read_byte(ZOffset::from(o.0.inc_by(4)));
+        Ok(ObjectNumber(u16::from(raw_number)))
     }
 
-    fn get_object_property(&self, o: Object, p: u8) -> Result<u16> // Is this right? Are all properties u16?
+    fn set_object_child(&self, o: ZObject, c: ObjectNumber) -> Result<()> {
+        // VNUM DEPEND
+        self.memory.borrow_mut().write_byte(o.0.inc_by(6), c.0 as u8)
+    }
+    fn set_object_sibling(&self, o: ZObject, s: ObjectNumber) -> Result<()> {
+        // VNUM DEPEND
+        self.memory.borrow_mut().write_byte(o.0.inc_by(5), s.0 as u8)
+    }
+    fn set_object_parent(&self, o: ZObject, p: ObjectNumber) -> Result<()> {
+        // VNUM DEPEND
+        self.memory.borrow_mut().write_byte(o.0.inc_by(4), p.0 as u8)
+    }
+
+    fn get_object_attribute(&self, o: ZObject, a: u8) -> Result<u8> {
+        // VNUM DEPEND
+        // range check.
+        let ba = o.0.inc_by(if a > 15 { 1} else { 0} );
+        let bitnum = a % 16;
+        let word = self.memory.borrow().read_word(ba);
+        Ok(((word >> (15 - bitnum)) & 0b1) as u8)
+    }
+    
+    fn set_object_attribute(&self, o: ZObject, a: u8, v: u8) -> Result<()> {
+        // VNUM DEPEND
+        // range check
+        let ba = o.0.inc_by(if a > 15 { 1 } else { 0 });
+        let word = self.memory.borrow().read_word(ba);
+        let bitnum = a % 16;
+        let the_bit = 1 << (15 - bitnum);
+        let new_word = if v == 0 {
+            word & !the_bit
+        } else {
+            word | the_bit
+        };
+
+        self.memory.borrow_mut().write_word(ba, new_word)
+    }
+
+    fn get_object_property(&self, o: ZObject, p: u8) -> Result<u16> // Is this right? Are all properties u16?
     {
         panic!("Unimplemented")
     }
-    fn set_object_property(&self, o: Object, p: u8, v: u16) -> Result<()> {
+
+    fn set_object_property(&self, o: ZObject, p: u8, v: u16) -> Result<()> {
         panic!("Unimplemented")
     }
 
